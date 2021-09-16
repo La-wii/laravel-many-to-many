2e63f75b46b096b14f2a9c7f44a9c090f2c7e7f3
La-wii <sentilavale@gmail.com>
2021-09-16 19:58:18 +0200

    first commit

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li> 
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('admin.posts.update',$post->id)}}" method="post">
+            @csrf
+            @method('PATCH')
+            <div class="mb-3">
+                <label for="titolo" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="titolo" name="title" value="{{ $post->title}}">
+            </div>
+
+            <div class="mb-3">
+                <label for="category" class="form-label">Categoria</label>
+                <select name="category_id" id="category" class="form-control">   
+                    <option value="">
+                        - - Seleziona una categoria - - 
+                    </option>
+                    @foreach($categories as $category)
+                        <option value="{{$category->id}}" 
+                        @if($category->id == old('category_id', $post->category->id))
+                        selected
+                        @endif>
+                        {{$category->name}}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            
+            <div class="mb-3">
+                <label for="desc" class="form-label">Descrizione</label>
+                <textarea class="form-control" name="content" id="desc" cols="30" rows="10">{{ $post->content}}</textarea>            
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
